acf984453350413df9cc44748b26415441d7b105
pr8986 <pkumarpatna4@gmail.com>
2024-06-19 23:58:32 +0530

    Authentication routes

--- models/user.js
+++ models/user.js
@@ -1,44 +1,77 @@
-const mongoose=require('mongoose');
-// const bcrypt=require('bcrypt');
+const mongoose = require('mongoose');
+const bcrypt=require('bcrypt');
 
-const userSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     },
-    age:{
-        type:String,
-        required:true
+    age: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String
+    email: {
+        type: String
     },
-    mobile:{
-        type:String
+    mobile: {
+        type: String
     },
-    adderss:{
-        type:String,
-        required:true
+    address: {
+        type: String,
+        required: true
     },
-    adharCardNumber:{
-        type:Number,
-        required:true,
-        unique:true
+    adharCardNumber: {
+        type: Number,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    role:{
-        type:String,
-        enum:['voter', 'admin'],
-        default:'voter'
+    role: {
+        type: String,
+        enum: ['voter', 'admin'],
+        default: 'voter'
     },
-    isVoted:{
+    isVoted: {
         type: Boolean,
-        default:false
+        default: false
     }
 });
 
-const User=mongoose.model('User',userSchema);
-module.exports=User;
+
+userSchema.pre('save', async function (next) {
+    const person = this;
+
+    // Hash the password only if it has been modified (or is new)
+    if (!person.isModified('password')) return next();
+
+    try {
+        // hash password generation
+        const salt = await bcrypt.genSalt(10);
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(person.password, salt);
+
+        // Override the plain password with the hashed one
+        person.password = hashedPassword;
+        next();
+    } catch (err) {
+        return next(err);
+    }
+})
+
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    try {
+        // Use bcrypt to compare the provided password with the hashed password
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    } catch (err) {
+        throw err;
+    }
+}
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
